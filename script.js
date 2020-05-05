b00aa89c8457ef32b90421bcb118cb2ab1e7bba2
Falsin <55051868+Falsin@users.noreply.github.com>
2020-05-05 09:53:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -363,8 +363,8 @@ window.addEventListener('orientationchange', function () {
 		calculator[1].classList.add('activeCalc');
 		//calculator[1].style.width = "100%";
 		//calculator[1].style.height = "100%";
-		container.style.width = '100%';
-		container.style.height = '100%';
+		container.style.width = '100vw';
+		container.style.height = '100vh';
 		calculator[1].style.border = "solid red 1px";			
 	}
 })
