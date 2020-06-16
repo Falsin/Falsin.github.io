8afd592987116f5cd2271b4b3453e2915eafcfd9
Artem Pitsil <greenldend@gmail.com>
2020-06-16 15:39:28 +0700

    small changes

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ window.onload = function () {
 }
 
 function setOrientation () {
-	/*let heightTest = test.clientHeight;
-	let widthTest  = test.clientWidth;*/
-	//alert(`в${test.clientHeight} + ш${test.clientWidth}`)
+	let heightTest = test.clientHeight;
+	let widthTest  = test.clientWidth;
+
 	if (!degree) {
 		for (let i = 0; i < fields.length; i++) {
 			fields[i].style.display = "none";
 		}
 		fields[0].style.display = "flex";
 
-		let width = Math.round(Math.min(test.clientHeight, test.clientWidth) / 5);
+		let width = Math.round(Math.min(heightTest, widthTest) / 5);
 		for (let i = 0; i < elements.length; i++) {
 			elements[i].style.width = width + 'px';
 		  	elements[i].style.height = width + 'px';
@@ -33,7 +33,7 @@ function setOrientation () {
 			fields[i].style.display = "none";
 		}
 		fields[1].style.display = "flex";
-		let height = Math.round(Math.min(test.clientHeight, test.clientWidth) / 6);
+		let height = Math.round(Math.min(heightTest, widthTest) / 6);
 	  	for (let i = 0; i < elements.length; i++) {
 			elements[i].style.width  = height + 'px';
 			elements[i].style.height = height + 'px';
@@ -42,11 +42,11 @@ function setOrientation () {
 
 }
 
+window.addEventListener(`resize`, () => setTimeout(setOrientation, 50), false);
+
 window.addEventListener('orientationchange', function () {
 	degree = window.orientation;
 	setTimeout(setOrientation, 50);
-	
-	//setOrientation();
 })
 
 /*
